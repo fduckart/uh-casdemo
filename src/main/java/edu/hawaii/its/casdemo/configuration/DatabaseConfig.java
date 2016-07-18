7b35a1e82fe7ff47699c559eb0f8542e059710b5
Frank Duckart <duckart@computer.org>
2016-07-18 09:54:12 -1000

    Spring configuration updates.

--- src/main/java/edu/hawaii/its/casdemo/configuration/DatabaseConfig.java
+++ src/main/java/edu/hawaii/its/casdemo/configuration/DatabaseConfig.java
@@ -1,5 +1,7 @@
 package edu.hawaii.its.casdemo.configuration;
 
+import java.util.Properties;
+
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -8,10 +10,20 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.util.Assert;
 
 @Configuration
+@PropertySources({
+        @PropertySource("classpath:META-INF/spring/general.properties"),
+        @PropertySource("classpath:META-INF/spring/database.properties"),
+        @PropertySource(value = "file://${override.properties.path}",
+                        ignoreResourceNotFound = true),
+})
 public class DatabaseConfig {
 
     @Value("${jdbc.url}")
@@ -31,6 +43,7 @@ public class DatabaseConfig {
         Assert.hasLength(url, "property 'url' is required");
         Assert.hasLength(username, "property 'user' is required");
         Assert.hasLength(driverClassName, "property 'driverClassName' is required");
+        Assert.hasLength(hibernateCacheProviderClass, "property 'hibernateCacheProviderClass' is required");
     }
 
     @Bean(name = "dataSource")
@@ -47,6 +60,43 @@ public class DatabaseConfig {
     @Bean
     public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
         return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setPersistenceXmlLocation("classpath:META-INF/persistence.xml");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setJpaProperties(jpaProperties());
+        em.setDataSource(dataSource());
+
+        return em;
+    }
+
+    @Value("${db.hibernate.dialect}")
+    private String hibernateDialect;
+
+    @Value("${db.hibernate.hbm2ddl.auto}")
+    private String hibernateHbm2ddlAuto;
+
+    @Value("${db.hibernate.cache.provider_class}")
+    private String hibernateCacheProviderClass;
+
+    @Value("${hibernate.connection.shutdown}")
+    private String hibernateConnectionShutdown;
+
+    @Value("${db.hibernate.show_sql}")
+    private String hibernateShowSql;
+
+    protected Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", hibernateDialect);
+        properties.setProperty("hibernate.hbm2ddl.auto", hibernateHbm2ddlAuto);
+        properties.setProperty("hibernate.cache.provider_class", hibernateCacheProviderClass);
+        properties.setProperty("hibernate.connection.shutdown", hibernateConnectionShutdown);
+        properties.setProperty("hibernate.show_sql", hibernateShowSql);
+
+        return properties;
     }
 
 }
