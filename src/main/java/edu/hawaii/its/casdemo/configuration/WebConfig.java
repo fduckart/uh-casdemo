4605258fd44aa1db29b067cbe4b1f92175d176c9
Frank Duckart <duckart@computer.org>
2016-07-29 11:42:49 -1000

    Fix incorrect default gate URL.

--- src/main/java/edu/hawaii/its/casdemo/configuration/WebConfig.java
+++ src/main/java/edu/hawaii/its/casdemo/configuration/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/").setViewName("gate");
     }
 
     @Bean
