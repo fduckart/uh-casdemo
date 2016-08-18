7fdf6b5e7c43de25b1b165b2394fce137b95944a
Frank Duckart <duckart@computer.org>
2016-08-17 16:42:06 -1000

    Added more controller tests.

--- src/test/java/edu/hawaii/its/casdemo/controller/HomeControllerSystemTest.java
+++ src/test/java/edu/hawaii/its/casdemo/controller/HomeControllerSystemTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import java.util.Locale;
 
@@ -14,13 +18,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
+import org.springframework.web.context.WebApplicationContext;
 
 import edu.hawaii.its.casdemo.configuration.CachingConfig;
 import edu.hawaii.its.casdemo.configuration.DatabaseConfig;
 import edu.hawaii.its.casdemo.configuration.WebConfig;
-import edu.hawaii.its.casdemo.security.UserContextServiceImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -30,17 +35,15 @@ public class HomeControllerSystemTest {
     @Autowired
     private HomeController homeController;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
     @Before
     public void setUp() {
-        homeController.setUserContextService(new UserContextServiceImpl() {
-            public String getCurrentUsername() {
-                return "duckart";
-            }
-
-            public Long getCurrentUhuuid() {
-                return new Long(89999999L);
-            }
-        });
+        mockMvc = webAppContextSetup(context)
+                .build();
     }
 
     @Test
@@ -58,4 +61,30 @@ public class HomeControllerSystemTest {
         assertTrue(model.asMap().keySet().contains("systemMessage"));
     }
 
+    @Test
+    public void attributes() {
+        Model model = new ExtendedModelMap();
+        assertEquals("attributes", homeController.attributes(Locale.US, model));
+    }
+
+    @Test
+    public void requestUrlDenied() throws Exception {
+        mockMvc.perform(get("/denied"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("denied"));
+    }
+
+    @Test
+    public void requestUrl404() throws Exception {
+        mockMvc.perform(get("/404"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+
+    @Test
+    public void requestUrlAttributes() throws Exception {
+        mockMvc.perform(get("/attributes"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("attributes"));
+    }
 }
