1b91663d2bd5e5849564f8611d0103db9b712262
Frank Duckart <duckart@computer.org>
2021-08-04 15:46:14 -1000

    Remove some unnecessary overrides.

--- src/test/java/edu/hawaii/its/casdemo/access/AssertionDummy.java
+++ src/test/java/edu/hawaii/its/casdemo/access/AssertionDummy.java
@@ -24,31 +24,6 @@ public class AssertionDummy implements Assertion {
     }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
-    }
-
-    @Override
     public Map<String, Object> getAttributes() {
         return null;
     }
