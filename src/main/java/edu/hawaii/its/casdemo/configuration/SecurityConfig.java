7b6b33744f0fc4ca37e9be055602ae80d53de66c
Frank Duckart <duckart@computer.org>
2021-10-15 22:07:33 -1000

    Remove unnecssary configuration.

--- src/main/java/edu/hawaii/its/casdemo/configuration/SecurityConfig.java
+++ src/main/java/edu/hawaii/its/casdemo/configuration/SecurityConfig.java
@@ -192,7 +192,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(casAuthenticationProvider());
-        auth.userDetailsService(userDetailsService());
     }
 
 }
