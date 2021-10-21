b1e32fd59210fa8de7125fc2d2c1a5a439183f5d
Frank Duckart <duckart@computer.org>
2021-10-21 12:10:01 -1000

    CAS configuration cleanup.

--- src/main/java/edu/hawaii/its/casdemo/access/CasUserDetailsService.java
+++ src/main/java/edu/hawaii/its/casdemo/access/CasUserDetailsService.java
@@ -1,41 +1,26 @@
 package edu.hawaii.its.casdemo.access;
 
 import java.util.Map;
-
-import javax.annotation.PostConstruct;
+import java.util.Objects;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jasig.cas.client.validation.Assertion;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.cas.userdetails.AbstractCasAssertionUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 
-@Service
-@Transactional
 public class CasUserDetailsService extends AbstractCasAssertionUserDetailsService {
 
     private static final Log logger = LogFactory.getLog(CasUserDetailsService.class);
 
-    @Autowired
-    private UserBuilder userBuilder;
+    private final UserBuilder userBuilder;
 
     // Constructor.
-    public CasUserDetailsService(@Autowired UserBuilder userBuilder) {
+    public CasUserDetailsService(UserBuilder userBuilder) {
         super();
+        Objects.requireNonNull(userBuilder, "UserBuilder cannot be null.");
         this.userBuilder = userBuilder;
-    }
-
-    @PostConstruct
-    public void init() {
-        logger.info("init; starting...");
-        logger.info("  userBuilder       : " + userBuilder);
-        Assert.notNull(userBuilder, "Property 'userBuilder' not set.");
-        logger.info("init; started.");
     }
 
     @Override
