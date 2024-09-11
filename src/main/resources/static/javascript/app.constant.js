9ae45a17fed5076f372d290fe2151953565ee990
Frank Duckart <duckart@computer.org>
2024-09-11 09:40:12 -1000

    wip

---
+++ src/main/resources/static/javascript/app.constant.js
@@ -0,0 +1,22 @@
+/*eslint no-undef: "error"*/
+
+(function() {
+    'use strict';
+
+    casdemoApp.constant('App', {
+        Campus: {
+            'MANOA': '7',
+            'SYSTEM': '11'
+        },
+        Url: {
+            FEEDBACK: '/casdemo/feedback/',
+            Api: {
+                CAMPUS: '/casdemo/api/campuses',
+                ERROR: '/casdemo/api/error/',
+                HOLIDAY: '/casdemo/api/holidays',
+                ROLE: '/casdemo/api/roles'
+            }
+        }
+    });
+
+})();
--- src/main/resources/static/javascript/app.js
+++ src/main/resources/static/javascript/app.js
@@ -1,20 +1,4 @@
 /*eslint no-undef: "error"*/
 
-"use strict";
-var casdemoApp = angular.module("casdemoApp", [ "ui.grid", "ui.grid.pagination" ]);
-
-casdemoApp.constant("App", {
-    Campus: {
-        "MANOA": "7",
-        "SYSTEM": "11"
-    },
-    Url: {
-        FEEDBACK: "/casdemo/feedback/",
-        Api: {
-            CAMPUS: "/casdemo/api/campuses",
-            ERROR: "/casdemo/api/error/",
-            HOLIDAY: "/casdemo/api/holidays",
-            ROLE: "/casdemo/api/roles"
-        }
-    }
-});
+const casdemoApp = angular.module('casdemoApp',
+    ['ui.grid', 'ui.grid.pagination']);
